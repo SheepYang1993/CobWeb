7c28f1d454727900670f8c93da146727fc4b8000
SheepYang <332594623@qq.com>
2020-11-26 15:44:38 +0800

    升级至androidx

--- app/src/main/java/me/sheepyang/cobweb/MainActivity.java
+++ app/src/main/java/me/sheepyang/cobweb/MainActivity.java
@@ -1,10 +1,10 @@
 package me.sheepyang.cobweb;
 
 import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
--- cobwebview/src/main/java/me/sheepyang/cobwebview/CobWebView.java
+++ cobwebview/src/main/java/me/sheepyang/cobwebview/CobWebView.java
@@ -6,7 +6,7 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
